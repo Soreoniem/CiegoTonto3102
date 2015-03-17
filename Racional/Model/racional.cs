9aa8752d8d0ead8ec043fce5ce5969aad0f134de
Soreoniem <j_l_juan@hotmail.com>
2015-03-17 19:18:57 +0100

    VisualStudio

---
+++ Racional/Model/racional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racional.Model
+{
+    class racional
+    {
+        private int numerador;
+        private int denominador;
+
+        public Racional(int numerador, int denominador)
+        {
+            this.numerador = numerador;
+            this.denominador = denominador;
+        }
+
+        public int getNumerador()
+        {
+            return
+        }
+    }
+}
